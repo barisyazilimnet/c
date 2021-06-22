844310b75adddff89d63a25e7a07ff4d1cc58e8a
BARIŞ KURT <kurt-bar07@hotmail.com>
2021-06-22 18:54:09 +0300

    commit

---
+++ renk.c
@@ -0,0 +1,50 @@
+
+
+#define ANSI_COLOR_RESET "\033[22;0m"
+#define ANSI_COLOR_BLACK "\033[22;30m"
+#define ANSI_COLOR_RED "\033[22;31m"
+#define ANSI_COLOR_GREEN "\033[22;32m"
+#define ANSI_COLOR_BROWN "\033[22;33m"
+#define ANSI_COLOR_BLUE "\033[22;34m"
+#define ANSI_COLOR_MAGENTA "\033[22;35m"
+#define ANSI_COLOR_CYAN "\033[22;36m"
+#define ANSI_COLOR_GRAY "\033[22;37m"
+#define ANSI_COLOR_DARK_GRAY "\033[01;30m"
+#define ANSI_COLOR_LIGHT_RED "\033[01;31m"
+#define ANSI_COLOR_LIGHT_GREEN "\033[01;32m"
+#define ANSI_COLOR_YELLOW "\033[01;33m"
+#define ANSI_COLOR_LIGHT_BLUE "\033[01;34m"
+#define ANSI_COLOR_LIGHT_MAGENTA "\033[01;35m"
+#define ANSI_COLOR_LIGHT_CYAN "\033[01;36m"
+#define ANSI_COLOR_WHITE "\033[01;37m"
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+	system("color 80");
+	
+//	printf(ANSI_COLOR_RESET"Merhaba\n");
+	printf(ANSI_COLOR_BLACK"Merhaba2\n");
+	printf(ANSI_COLOR_RED"Merhaba3\n");
+	printf(ANSI_COLOR_GREEN"Merhaba4\n");
+	printf(ANSI_COLOR_BROWN"Merhaba5\n");
+	printf(ANSI_COLOR_BLUE"Merhaba6\n");
+	printf(ANSI_COLOR_MAGENTA"Merhaba7\n");
+	printf(ANSI_COLOR_CYAN"Merhaba8\n");
+	printf(ANSI_COLOR_GRAY"Merhaba9\n");
+	printf(ANSI_COLOR_DARK_GRAY"Merhaba10\n");
+	printf(ANSI_COLOR_LIGHT_RED"Merhaba11\n");
+	printf(ANSI_COLOR_LIGHT_GREEN"Merhaba12\n");
+	printf(ANSI_COLOR_YELLOW"Merhaba13\n");
+	printf(ANSI_COLOR_LIGHT_BLUE"Merhaba14\n");
+	printf(ANSI_COLOR_LIGHT_MAGENTA"Merhaba15\n");
+	printf(ANSI_COLOR_LIGHT_CYAN"Merhaba16\n");
+	printf(ANSI_COLOR_WHITE"Merhaba17");
+}
+
+
